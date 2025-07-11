59e26dec91ef1c5447834be66f6fbd104f714484
agusnieto <santiago-n@live.com.ar>
2025-07-11 05:27:39 -0300

    remove viejo

--- i3/volume.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-VOL=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -o '[0-9]*%')
-MUTE=$(pactl get-sink-mute @DEFAULT_SINK@ | grep -o 'yes\|no')
-
-if [ "$MUTE" == "yes" ]; then
-    echo "󰝟  $VOL"
-else
-    echo "󰕾  $VOL"
-fi
