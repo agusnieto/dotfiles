40cfb39eb1a2c681b72112e7b0b2d19a8015fba6
agusnieto <santiago-n@live.com.ar>
2025-08-28 13:21:24 -0300

    waybar modular

---
+++ River/scripts/waybar/battery.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+bat="BAT0"
+cat /sys/class/power_supply/$bat/capacity
+
---
+++ River/scripts/waybar/picker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Uso: porcentaje "a b c d e"
+# Ej: ./picker.sh 95 "a b c d e"
+
+percent=$1
+shift
+read -r -a icons <<< "$@"
+
+len=${#icons[@]}
+
+idx=$(( percent * len / 100 ))
+
+if (( idx >= len )); then
+  idx=$((len - 1))
+fi
+
+echo "${icons[$idx]}"
+
