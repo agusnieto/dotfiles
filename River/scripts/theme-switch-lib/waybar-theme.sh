5fcb67b47b79e1ebcd568e57913a86279891437b
agusnieto <santiago-n@live.com.ar>
2025-09-05 20:32:50 -0300

    waybar script update

--- River/scripts/theme-switch-lib/waybar-theme.sh
+++ River/scripts/theme-switch-lib/waybar-theme.sh
@@ -8,33 +8,31 @@ structure="$waybar_dir/base.css"
 output="$waybar_dir/style.css"
 
 if [ ! -f "$colors" ]; then
-	echo "Waybar: theme $theme not found!"
-	exit 1
+    echo "Waybar: theme $theme not found!"
+    exit 1
 fi
 
-get_color() {
-	sed -n "${1}p" "$colors"
-}
-
-# Colores por renglon
-white=$(get_color 16)
-black=$(get_color 1)
-red=$(get_color 2)
-unfocus=$(get_color 9)
-focused=$(get_color 16)
-base=$(get_color 17)
-special=$(get_color 18)
+names=(
+    black red green yellow blue purple aqua gray brgray brred brgreen bryellow brblue brpurple braqua white bg2 special
+)
 
 {
-	echo "@define-color white $white;"
-	echo "@define-color black $black;"
-	echo "@define-color red $red;"
-	echo "@define-color unfocus $unfocus;"
-	echo "@define-color focused $focused;"
-	echo "@define-color base $base;"
-	echo "@define-color special $special;"
+	echo "/* Colors ($theme) */"
+	i=0
+	while IFS= read -r color; do
+		name="${names[$i]}"
+		printf "@define-color %-8s %s;\n" "$name" "$color"
+		((i++))
+	done < "$colors"
 	echo ""
 	cat "$structure"
 } > "$output"
+
+echo "Waybar: reloading"
+
+killall waybar
+sleep 1
+echo "Waybar: starting waybar"
+riverctl spawn waybar
 
 echo "Waybar: using $theme theme."
